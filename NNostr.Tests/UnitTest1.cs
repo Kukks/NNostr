e4a9c416472584682edec59acfd556017bb5d718
Kukks <evilkukka@gmail.com>
2022-02-14 18:55:46 +0100

    fix  test

--- NNostr.Tests/UnitTest1.cs
+++ NNostr.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using NBitcoin.Secp256k1;
 using NNostr.Client;
 using Xunit;
@@ -16,7 +17,7 @@ public class UnitTest1
        Assert.Equal("7cef86754ddf07395c289c30fe31219de938c6d707d6b478a8682fc75795e8b9", pubKey.ToBytes().ToHex());
     }
     [Fact]
-    public void CanHandleNIP04()
+    public async Task CanHandleNIP04()
     {
         var user1 =  CreateUser("7f4c11a9742721d66e40e321ca50b682c27f7422190c14a187525e69e604836a");
         var user2 =  CreateUser("203b892f1d671fec43a04b36c452de631c9cf55b7a93b75d97ff1e41d217f038");
@@ -37,9 +38,9 @@ public class UnitTest1
             }
         };
         
-        NIP04.EncryptNip04Event(evtFromUser1ToUser2, user1.privateKey);
-        Assert.Equal("test", NIP04.DecryptNip04Event(evtFromUser1ToUser2, user2.privateKey));
-        Assert.Equal("test", NIP04.DecryptNip04Event(evtFromUser1ToUser2, user1.privateKey));
+        await NIP04.EncryptNip04Event(evtFromUser1ToUser2, user1.privateKey);
+        Assert.Equal("test", await NIP04.DecryptNip04Event(evtFromUser1ToUser2, user2.privateKey));
+        Assert.Equal("test", await NIP04.DecryptNip04Event(evtFromUser1ToUser2, user1.privateKey));
         
        }
 
