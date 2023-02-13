762526ac59ffc36f8be2de04489dbdb24699e253
Kori Francis <kfrancis@clinicalsupportsystems.com>
2023-02-12 20:29:45 -0500

    Adds #9

---
+++ NNostr.Client/Protocols/NIP05.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NNostr.Client.Protocols
+{
+    public class NIP05Response
+    {
+        [JsonPropertyName("names")]
+        public Dictionary<string, string> Names { get; set; }
+
+        [JsonPropertyName("relays")]
+        public Dictionary<string, List<string>> Relays { get; set; }
+    }
+
+    /// <summary>
+    /// https://github.com/nostr-protocol/nips/blob/master/05.md
+    /// </summary>
+    /// <param name="Username"></param>
+    /// <param name="Host"></param>
+    public class NIP05
+    {
+        public string Username { get; set; }
+        public string Host { get; set; }
+
+        public NIP05(string username, string host)
+        {
+            Username = username;
+            Host = host;
+        }
+
+        public Uri Url => new("https://" + Host + "/.well-known/nostr.json?name=" + Username);
+
+        public Uri SiteUrl => new("https://" + Host);
+
+        public static NIP05? Parse(string nip05)
+        {
+            string[] parts = nip05.Split('@');
+            if (parts.Length != 2)
+            {
+                return null;
+            }
+            return new NIP05(parts[0], parts[1]);
+        }
+
+        public static async Task<NIP05?> Validate(string pubkey, string nip05_str)
+        {
+            var nip05 = NIP05.Parse(nip05_str);
+            if (nip05 == null)
+            {
+                return null;
+            } 
+
+            if (nip05.Url == null)
+            {
+                return null;
+            }
+
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(nip05.Url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var decoded = await JsonSerializer.DeserializeAsync<NIP05Response>(stream);
+
+            if (decoded?.Names == null || !decoded.Names.ContainsKey(nip05.Username))
+            {
+                return null;
+            }
+
+            if (decoded.Names[nip05.Username] != pubkey)
+            {
+                return null;
+            }
+
+            return nip05;
+        }
+    }
+}
---
+++ NNostr.Tests/Nip5Tests.cs
@@ -0,0 +1,29 @@
+﻿using NNostr.Client.Protocols;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NNostr.Tests
+{
+    public class Nip5Tests
+    {
+        [Theory]
+        [InlineData("kori@nostress.cc", "1ad34e8aa265df5bd6106b4535a6a82528141efd800beb35b6413d7a8298741f")]
+        [InlineData("kori@no.str.cr", "1ad34e8aa265df5bd6106b4535a6a82528141efd800beb35b6413d7a8298741f")]
+        public async Task CanGetNip5(string nip5, string pubKey)
+        {
+            NIP05.Parse(nip5).ShouldSatisfyAllConditions(
+                x => x.ShouldNotBeNull()
+            );
+
+            var result = await NIP05.Validate(pubKey, nip5);
+            result.ShouldSatisfyAllConditions(
+                x => x.ShouldNotBeNull()    
+            );
+        }
+    }
+}
